daf825e50cc62ba92eec8aea7660bdf6d4179956
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-07-27 09:54:29 -0300

    fix debit

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -19,9 +19,9 @@ BillingCycle.route('count', (req, res, next) => {
 
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate({
-        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debits.value" } }
     }, {
-            $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+            $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debit" } }
         }, {
             $project: { _id: 0, credit: 1, debt: 1 }
         }, (error, result) => {
