d9f47e9ef13aebbf7c5fc5e3b0a830ed044eb128
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-07-29 09:17:37 -0300

    backend funcional ok

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -19,16 +19,16 @@ BillingCycle.route('count', (req, res, next) => {
 
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate({
-        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debits.value" } }
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
     }, {
-            $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debit" } }
+            $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
         }, {
-            $project: { _id: 0, credit: 1, debit: 1 }
+            $project: { _id: 0, credit: 1, debt: 1 }
         }, (error, result) => {
             if (error) {
                 res.status(500).json({ errors: [error] })
             } else {
-                res.json(result[0] || { credit: 0, debit: 0 })
+                res.json(result[0] || { credit: 0, debt: 0 })
             }
         })
 })
